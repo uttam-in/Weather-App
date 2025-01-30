528d39f0496ae11cf9c0ba11f1f8b91b1020ba92
Uttam Kumar P <panasalauttam@gmail.com>
2025-01-30 17:58:47 -0600

    add initial exports for components, hooks, and styles

---
+++ src/app/layout.tsx
@@ -0,0 +1,64 @@
+import 'src/global.css';
+
+// ----------------------------------------------------------------------
+
+import type { Viewport } from 'next';
+
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+
+import { CONFIG } from 'src/config-global';
+import { primary } from 'src/theme/core/palette';
+import { schemeConfig } from 'src/theme/scheme-config';
+import { ThemeProvider } from 'src/theme/theme-provider';
+
+import { ProgressBar } from 'src/components/progress-bar';
+import { MotionLazy } from 'src/components/animate/motion-lazy';
+import { SettingsDrawer, defaultSettings, SettingsProvider } from 'src/components/settings';
+
+import { AuthProvider } from 'src/auth/context/jwt';
+
+// ----------------------------------------------------------------------
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: primary.main,
+};
+
+export const metadata = {
+  icons: [
+    {
+      rel: 'icon',
+      url: `${CONFIG.assetsDir}/favicon.ico`,
+    },
+  ],
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default async function RootLayout({ children }: Props) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript
+          defaultMode={schemeConfig.defaultMode}
+          modeStorageKey={schemeConfig.modeStorageKey}
+        />
+
+        <AuthProvider>
+          <SettingsProvider settings={defaultSettings}>
+            <ThemeProvider>
+              <MotionLazy>
+                <ProgressBar />
+                <SettingsDrawer />
+                {children}
+              </MotionLazy>
+            </ThemeProvider>
+          </SettingsProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
